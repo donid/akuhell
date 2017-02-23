b6eff2cf6626bb147af79c277c2cf6a595c2fce0
donid <fk@fk.com>
2017-02-23 19:20:18 +0100

    Key combinations with AltGr should now work with Linux hosts, too (Issue #1)

--- src/keyboard_layouts/keyboard_layouts.ino
+++ src/keyboard_layouts/keyboard_layouts.ino
@@ -11,13 +11,15 @@ void setup()
 {
   pinMode(buttonPin, INPUT);  // Set the button as an input
   digitalWrite(buttonPin, HIGH);  // Pull the button high
+  //Serial.begin(9600);
+  //Serial.println("akuhell initialize v1");
 }
 
 void loop()
 {
   if (digitalRead(buttonPin) == 0)  // if the button goes low
   {
-    Serial.println("button low");
+    Serial.println("button low (v1)");
 
     String s1="h§öß";
     String s2=utf8_charAt(s1,2);
@@ -33,7 +35,7 @@ void loop()
 
     Keyboard_writeUtf8String(" ^°1!2²\"3§³4$5%6&7/{8([9)]0=}ß?\\´`qQ@wWeE€rRtTzZuUiIoOpPüÜ+*~aAsSdDfFgGhHjJkKlLöÖäÄ#'<>|yYxXcCvVbBnNmMµ,;.:-_");
    
-    delay(1000);  // delay so there aren't a kajillion z's
+    delay(1000);
   }
 }
 
